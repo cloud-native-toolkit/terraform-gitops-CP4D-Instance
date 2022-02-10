694a8537c4c44b357700d6a3d4b7e4cd1989f0ef
Andrew Trice <amtrice@us.ibm.com>
2022-02-10 12:55:21 -0500

    re-enabled cp4d-instance
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- test/stages/stage4-outputs.tf
+++ test/stages/stage4-outputs.tf
@@ -1,18 +1,18 @@
 
-# resource null_resource write_outputs {
-#   provisioner "local-exec" {
-#     command = "echo \"$${OUTPUT}\" > gitops-output.json"
+resource null_resource write_outputs {
+  provisioner "local-exec" {
+    command = "echo \"$${OUTPUT}\" > gitops-output.json"
 
-#     environment = {
-#       OUTPUT = jsonencode({
-#         name        = module.cp4d-instance.name
-#         branch      = module.cp4d-instance.branch
-#         namespace   = module.cp4d-instance.namespace
-#         server_name = module.cp4d-instance.server_name
-#         layer       = module.cp4d-instance.layer
-#         layer_dir   = module.cp4d-instance.layer == "infrastructure" ? "1-infrastructure" : (module.cp4d-instance.layer == "services" ? "2-services" : "3-applications")
-#         type        = module.cp4d-instance.type
-#       })
-#     }
-#   }
-# }
+    environment = {
+      OUTPUT = jsonencode({
+        name        = module.cp4d-instance.name
+        branch      = module.cp4d-instance.branch
+        namespace   = module.cp4d-instance.namespace
+        server_name = module.cp4d-instance.server_name
+        layer       = module.cp4d-instance.layer
+        layer_dir   = module.cp4d-instance.layer == "infrastructure" ? "1-infrastructure" : (module.cp4d-instance.layer == "services" ? "2-services" : "3-applications")
+        type        = module.cp4d-instance.type
+      })
+    }
+  }
+}
