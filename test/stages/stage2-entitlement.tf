0d77dd470e7977302e27db3357e7af2c4f1dda51
Andrew Trice <amtrice@us.ibm.com>
2022-11-10 11:51:49 -0500

    fixed gitops_repo reference
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- test/stages/stage2-entitlement.tf
+++ test/stages/stage2-entitlement.tf
@@ -20,9 +20,9 @@ module "gitops_global_pullsecret_synch" {
   ]
   source = "github.com/cloud-native-toolkit/terraform-gitops-global-pullsecret-synch"
 
-  git_credentials = module.gitops_repo.git_credentials
-  gitops_config = module.gitops_repo.gitops_config
-  kubeseal_cert = module.gitops_repo.sealed_secrets_cert
+  git_credentials = module.gitops.git_credentials
+  gitops_config = module.gitops.gitops_config
+  kubeseal_cert = module.gitops.sealed_secrets_cert
   namespace = module.gitops_namespace.name
-  server_name = module.gitops_repo.server_name
+  server_name = module.gitops.server_name
 }
